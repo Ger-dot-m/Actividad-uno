5b46aa3534a7ea4114577bcb7673a747924cb3b2
Germain García <72660021+Ger-dot-m@users.noreply.github.com>
2021-09-14 23:58:58 -0500

    Estado inicial
    Aqui añado los containers redondeados.

--- startup_dos/lib/main.dart
+++ startup_dos/lib/main.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(FoodApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Welcome to Flutter'),
+          ),
+          body: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              MainWidget("Persona1"),
+              MainWidget("Persona2"),
+              MainWidget("Persona2"),
+              MainWidget("Persona2"),
+              MainWidget("Persona2"),
+            ],
+          ))),
+    );
+  }
 }
 
-class FoodApp extends StatelessWidget{
+class MainWidget extends StatelessWidget {
+  String texto = "";
+  MainWidget(this.texto);
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(){
-      home: MainApp(),
-    };
+  Widget build(BuildContext context) {
+    return Container(
+        width: 300,
+        height: 150,
+        child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Container(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/tux.jpg"),
+                  Text(texto, style: TextStyle(fontSize: 30),
+                  )
+                ],
+              ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.red,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20))))));
   }
 }
