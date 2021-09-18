b652341df40ea7ce934f2c1b015bdbf5d1e46323
Germain García <72660021+Ger-dot-m@users.noreply.github.com>
2021-09-18 07:51:28 -0500

    Update main.dart
    Ya tiene decoración de sombra y contorno el widget principal.

--- startup_dos/lib/main.dart
+++ startup_dos/lib/main.dart
@@ -20,8 +20,6 @@ class MyApp extends StatelessWidget {
               MainWidget("Persona1"),
               MainWidget("Persona2"),
               MainWidget("Persona2"),
-              MainWidget("Persona2"),
-              MainWidget("Persona2"),
             ],
           ))),
     );
@@ -37,20 +35,25 @@ class MainWidget extends StatelessWidget {
     return Container(
         width: 300,
         height: 150,
+        margin: EdgeInsets.all(40),
+        decoration: BoxDecoration(
+          color: Colors.amber,
+            border: Border.all(
+              color: Colors.amber,
+            ),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.black54,
+                  blurRadius: 15.0,
+                  offset: Offset(0.0, 0.75))
+            ],
+            borderRadius: BorderRadius.all(Radius.circular(10))),
         child: Padding(
             padding: EdgeInsets.all(20.0),
             child: Container(
               child: Column(
-                children: [
-                  Image.asset("assets/images/tux.jpg"),
-                  Text(texto, style: TextStyle(fontSize: 30),
-                  )
-                ],
+                children: [Image.asset("assets/images/tux.jpg")],
               ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.red,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(20))))));
+            )));
   }
 }
