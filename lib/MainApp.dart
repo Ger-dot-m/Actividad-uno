d891db70c83ef62dbc64ee8f18984e992412ab8d
Germain García <72660021+Ger-dot-m@users.noreply.github.com>
2021-10-30 13:45:07 -0500

    Add files via upload

--- lib/MainApp.dart
+++ lib/MainApp.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'MainWidget.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+  MyApp({Key ?key}) : super(key: key);
+  _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  List<MainWidget> widgets = [];
+
+  Future<void> readJson() async{
+    final String response = await rootBundle.loadString("assets/JSON/comidas.json");
+    final datos = await json.decode(response);
+
+    for(int i = 0; i<10;i++){
+      widgets.add(MainWidget( datos[i]["nombre"],
+                              datos[i]["descripcion"],
+                              datos[i]["precio"],
+                              datos[i]["imagen"]  ));
+    }
+    setState(() {
+      widgets;
+    });
+  }
+
+  @override
+  void initState(){
+    readJson();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,85 +49,14 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.orange[700],
           ),
           body: Center(
-              child: ListView(
-            children: <Widget>[
-              MainWidget(
-                  "Enchiladas rancheras",
-                  "Cerdo cocido a fuego lento, cebolla, pimiento, queso, salsa casera, arroz mexicano, crema agria, lechuga, guacamole.",
-                  "90.00",
-                  "assets/images/enchiladas.jpeg"),
-              MainWidget(
-                  "Chimichanga",
-                  "Pollo marinado, pimiento, pico de gallo, queso, arroz mexicano, guacamole, crema agria y frijoles negros.",
-                  "70.00",
-                  "assets/images/chimichangas.jpg"),
-              MainWidget(
-                  "Burrito",
-                  "La carne que tú elijas (res, cerdo o pollo) o vegetariano; cebolla, arroz mexicano, pimiento, queso, frijoles negros picantes.",
-                  "45.00",
-                  "assets/images/burritos.jpg"),
-              MainWidget(
-                  "Pollo en chipotle",
-                  "Pechuga de pollo adobada, jamón, queso, cebolla asada, jitomate, champiñones, arroz mexicano, frijoles refritos.",
-                  "110.00",
-                  "assets/images/pollo.jpg"),
-              MainWidget(
-                  "Pez dorado mexicano",
-                  "Pez dorado, aguacate, salsa de aguacate con chile serrano, mantequilla de hierbas y cítricos, camarones, guacamole.",
-                  "180.00",
-                  "assets/images/pez_dorado.jpg"),
-              MainWidget(
-                  "Filete con chimichurri",
-                  "Filete de falda de res, salsa chimichurri casera, ensalada fresca, arroz mexicano y frijoles refritos picantes.",
-                  "220.00",
-                  "assets/images/filete.jpg"),
-              MainWidget(
-                  "Tostadas mexicanas",
-                  "3 tortillas de tacos doradas, frijol refrito, crema, lechuga, tomate, queso mozzarella, salsa jalapeña, salsa aurora, carne de su elección.",
-                  "220.00",
-                  "assets/images/tostadas.jpg"),
-              MainWidget(
-                  "Zarape",
-                  "Combinación de carne, pollo, res, pastor, jamón, tomate, cebolla, chile jalapeño, queso mozzarella, salsa jalapeña, 6 tortillas de taco.",
-                  "100.00",
-                  "assets/images/zarape.jpg"),
-              MainWidget(
-                  "Guaraches",
-                  "Orden de 2 guaraches con frijoles y arroz ranchero, aguacate, lechuga, carne a su elección, cuajada y crema, salsa ranchera, cebolla con cilantro.",
-                  "70.00",
-                  "assets/images/guaraches.jpg"),
-              MainWidget(
-                  "Gordita",
-                  "Ricas gorditas rellanas de frijol, aguacate, crema, lechuga, queso mozarela, carene de elección, res, pollo, posta, mixto, chorizo, salsa ranchera, salsa jalapeña, cebolla con cilantro. ",
-                  "50.00",
-                  "assets/images/gorditas.png"),
-              MainWidget(
-                  "Alambre",
-                  "Fajitas de carne asada o pollo gratinado, queso mozzarella, chile jalapeño, cebolla, 6 tortillas de taco, salsa jalapeña, limón. ",
-                  "120.00",
-                  "assets/images/alambre.jpg"),
-              MainWidget(
-                  "Quesadillas",
-                  "Tortillas de harina rellenas de queso Mozzarella, frijol, guacamol, salsa Aurora, crema y salsa jalapeña. ",
-                  "80.00",
-                  "assets/images/quesadillas.jpg"),
-              MainWidget(
-                  "Sopa de tortilla",
-                  "Sopa de tortillas doradas con fondo de tomates, especies aromáticas, aguacate, crema, queso Mozzarella, cebolla con cilantro, salsa jalapeña y la carne de su elección.",
-                  "70.00",
-                  "assets/images/sopa_de_tortillas.jpg"),
-              MainWidget(
-                  "Frijoladas mexicanas",
-                  "Tortillas de harina rellenas de queso Mozzarella, frijol, guacamol. Salsa Aurora, crema y salsa jalapeña. ",
-                  "90.00",
-                  "assets/images/frijoladas.jpg"),
-              MainWidget(
-                  "Cebollines",
-                  "Chile jalapeño, salsa jalapeño y limón.  ",
-                  "30.00",
-                  "assets/images/cebollines.jpg"),
-            ],
-          ))),
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: widgets.length,
+                  itemBuilder: (context,index){
+                    return widgets[index];
+                  }
+              ))),
     );
   }
 }
